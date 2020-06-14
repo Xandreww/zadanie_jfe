a392cdcce7f09f26dfd5e0a9aa3091a9e3365777
Andrzej Popek <andrzejpopek93@gmail.com>
2020-06-14 10:28:09 +0200

    add clear button, fix subscribers listener

--- static/functions/addEventListeners.js
+++ static/functions/addEventListeners.js
@@ -1,19 +1,33 @@
 import channels from "../channels.js";
 import loadContent from "../functions/loadContent.js";
+import toImperialNotation from "../functions/toImperialNotation.js";
 
 const addEventListeners = () => {
   const title = document.getElementById("title");
+  const titleButton = document.getElementById("sort-title");
   const subscribers = document.getElementById("subscribers");
+  const subscribersButton = document.getElementById("sort-subscribers");
   const videos = document.getElementById("videos");
+  const videosButton = document.getElementById("sort-videos");
   const views = document.getElementById("views");
+  const viewsButton = document.getElementById("sort-views");
+  const sortButtons = [
+    titleButton,
+    subscribersButton,
+    videosButton,
+    viewsButton,
+  ];
+  const clear = document.getElementById("clear");
 
   title.addEventListener("change", () => {
     console.log("Sort by title");
   });
 
   subscribers.addEventListener("change", () => {
-    console.log("Sort by subscribers");
-    sortBySubs(channels);
+    if ((subscribersButton.checked = true)) {
+      console.log("Sort by subscribers");
+      sortBySubs(channels);
+    }
   });
 
   videos.addEventListener("change", () => {
@@ -22,6 +36,13 @@ const addEventListeners = () => {
 
   views.addEventListener("change", () => {
     console.log("Sort by views");
+  });
+
+  clear.addEventListener("click", () => {
+    for (let sortButton of sortButtons) {
+      sortButton.checked = false;
+    }
+    loadContent(toImperialNotation(channels));
   });
 
   const prepareNumbers = (x) => {
--- static/script.js
+++ static/script.js
@@ -7,44 +7,4 @@ window.onload = () => {
   loadContent(toImperialNotation(channels));
 };
 
-// const addEventListeners = () => {
-//   const title = document.getElementById("title");
-//   const subscribers = document.getElementById("subscribers");
-//   const videos = document.getElementById("videos");
-//   const views = document.getElementById("views");
-
-//   title.addEventListener("change", () => {
-//     console.log("Sort by title");
-//   });
-
-//   subscribers.addEventListener("change", () => {
-//     console.log("Sort by subscribers");
-//     sortBySubs(channels);
-//   });
-
-//   videos.addEventListener("change", () => {
-//     console.log("Sort by videos");
-//   });
-
-//   views.addEventListener("change", () => {
-//     console.log("Sort by views");
-//   });
-
-//   const prepareNumbers = (x) => {
-//     return parseInt(x.replace(/[., ]/g, ""));
-//   };
-
-//   const sortBySubs = (array) => {
-//     let sortedBySubs = [...array];
-//     sortedBySubs.sort((a, b) => {
-//       return (
-//         prepareNumbers(b.statistics.subscriberCount) -
-//         prepareNumbers(a.statistics.subscriberCount)
-//       );
-//     });
-
-//     loadContent(sortedBySubs);
-//   };
-// };
-
 addEventListeners();
